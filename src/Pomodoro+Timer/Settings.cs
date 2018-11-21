5aef03e303b1639941052c63dcddaebd1b6967c8
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2018-11-22 00:04:41 +0100

    Add settings

---
+++ src/Pomodoro+Timer/Settings.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Windows.Storage;
+
+namespace PomodoroTimer {
+
+    public static class Settings {
+
+        private static ApplicationDataContainer Local {
+            get => ApplicationData.Current.LocalSettings;
+        }
+
+        private const string PomodoroRunLengthKey = "pomodoroLength";
+
+        public static TimeSpan PomodoroRunLength {
+            get {
+                if (!Local.Values.ContainsKey(PomodoroRunLengthKey))
+                    return TimeSpan.FromMinutes(25);
+                else
+                    return new TimeSpan((long)Local.Values[PomodoroRunLengthKey]);
+            }
+            set {
+                Local.Values[PomodoroRunLengthKey] = value.Ticks;
+            }
+        }
+
+        private const string RestRunLengthKey = "restLength";
+
+        public static TimeSpan RestRunLength {
+            get {
+                if (!Local.Values.ContainsKey(RestRunLengthKey))
+                    return TimeSpan.FromMinutes(5);
+                else
+                    return new TimeSpan((long)Local.Values[RestRunLengthKey]);
+            }
+            set {
+                Local.Values[RestRunLengthKey] = value.Ticks;
+            }
+        }
+
+    }
+
+}
